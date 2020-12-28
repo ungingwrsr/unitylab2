afe9efb6ad0accc9db4e4f1e324d02ee4bc0831d
ungingwrsr <58808922+ungingwrsr@users.noreply.github.com>
2020-12-28 10:12:03 +0700

    Introduction to functions

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -8,23 +8,34 @@ public class SpawnerSample : MonoBehaviour
     void Start()
     {
         int angle = 15;
+        int radius = 5;
         Vector3 spawnPosition = transform.position;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 55;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 95;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        angle = 135;
+        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
+    void AddingNumbers(float num1, float num2)
+    {
+        float resultingNumber;
+        resultingNumber = num1 + num2;
+    }
 
-    
 }
+
 
